57ca6b1a393b6ae167d8d3813106605b9bccece8
Jose Cavani <Jose.Cavani@gmail.com>
2022-02-18 11:42:06 -0300

    commit

--- IntepretadorDeNumeros.ConsoleApp1/Program.cs
+++ IntepretadorDeNumeros.ConsoleApp1/Program.cs
@@ -6,12 +6,11 @@ namespace IntepretadorDeNumeros.ConsoleApp1
     {
         static void Main(string[] args)
         {
-            #region declarasão de variaveis
+            #region declaracão de variaveis
             string entrada = @"
      __  __      __  __  __  __  __  __ 
    | __| __||__||__ |__    ||__||__||  |
    ||__  __|   | __||__|   ||__| __||__|
-   
 
     ";
             int contador = 0;
@@ -21,9 +20,17 @@ namespace IntepretadorDeNumeros.ConsoleApp1
             linhas[1] = leitorDeCodigos.ReadLine();
             linhas[2] = leitorDeCodigos.ReadLine();
             linhas[3] = leitorDeCodigos.ReadLine();
-            char[] charParaLer1 = new char[4];
-            char[] charParaLer2 = new char[4];
-            char[] charParaLer3 = new char[4];
+            string linhaParaLer = "";
+
+
+
+
+
+            /* idea antiga
+            //char[] charParaLer1 = new char[4]; 
+            //char[] charParaLer2 = new char[4];
+            //char[] charParaLer3 = new char[4];
+            */
             #endregion
 
 
@@ -36,13 +43,17 @@ namespace IntepretadorDeNumeros.ConsoleApp1
         #endregion
 
 
-        #region codigo para dividir o elefante ou seja colocar os numeros para os chars
+        #region codigo para dividir colocar os numeros em uma so linha (isso faz um simbolo unico para cada numero) 
         checkagem:
 
-                charParaLer1 = linhas[1].Substring(contador, 4).ToCharArray();
-                charParaLer2 = linhas[2].Substring(contador, 4).ToCharArray();
-                charParaLer3 = linhas[3].Substring(contador, 4).ToCharArray();
-                contador += 4;
+            linhaParaLer = linhas[1].Substring(contador, 4) + linhas[2].Substring(contador, 4) + linhas[3].Substring(contador, 4);
+
+
+            //idea antiga
+            //charParaLer1 = linhas[1].Substring(contador, 4);
+            //charParaLer2 = linhas[2].Substring(contador, 4);
+            //charParaLer3 = linhas[3].Substring(contador, 4);
+            contador += 4;
             // leia os 4 posições para frente de cada linha depois marque essa posição com o cotador
             //dai faça novamente depois com o novo valor do contador
 
@@ -51,6 +62,16 @@ namespace IntepretadorDeNumeros.ConsoleApp1
 
             #region codigo para ver o que os charParaLer estao lendo habilitar para desbugar
             //codigo para ver o que os charParaLer estao lendo
+            //Console.WriteLine(linhaParaLer);
+
+
+
+
+
+
+
+
+            //idea antiga
             //foreach (var item in charParaLer1)
             //{
             //    Console.Write(item);
@@ -69,55 +90,93 @@ namespace IntepretadorDeNumeros.ConsoleApp1
             #endregion
 
             #region codigo que ve qual numero é
-            if (charParaLer2[0] == ' ' && charParaLer1[1] == ' ' && charParaLer2[3] == '|' && charParaLer3[3] == '|')
+
+            switch (linhaParaLer)
+            {
+                case "       |   |":
                     Console.Write(1);
-
-           
-
-            
-               else if (charParaLer1[1] == '_' && charParaLer1[2] == '_' && charParaLer2[1] == '_' && charParaLer2[0] == ' ' && charParaLer2[2] == '_' && charParaLer2[3] == '|' && charParaLer3[0] == '|' && charParaLer3[1] == '_' && charParaLer3[2] == '_')
+                    break;
+                case " __  __||__ ":
                     Console.Write(2);
-
-            
-
-           
-               else if (charParaLer1[0] == ' ' && charParaLer1[1] == '_' && charParaLer1[2] == '_' && charParaLer1[3] == ' ' && charParaLer2[0] == ' ' && charParaLer2[1] == '_' && charParaLer2[2] == '_' && charParaLer2[3] == '|' && charParaLer3[0] == ' ' && charParaLer3[1] == '_' && charParaLer3[2] == '_' && charParaLer3[3] == '|')
+                    break;
+                case " __  __| __|":
                     Console.Write(3);
-
-            
-
-           
-               else if (charParaLer2[0] == '|' && charParaLer2[1] == '_' && charParaLer2[2] == '_'&& charParaLer2[3] == '|' && charParaLer3[2] == ' ')
+                    break;
+                case "    |__|   |":
                     Console.Write(4);
-
-           
-           
-               else if ( charParaLer1[2] == '_' && charParaLer1[3] == ' ' && charParaLer2[2] == '_' && charParaLer2[3] == ' ' && charParaLer2[1] == '_' && charParaLer3[3] == '|' && charParaLer3[1] == '_' && charParaLer3[0] == ' ')
-                    Console.Write(5);       
-           
-               else if (charParaLer1[2] == '_' && charParaLer1[3] == ' ' && charParaLer2[2] == '_' && charParaLer2[3] == ' ' && charParaLer2[1] == '_' && charParaLer3[3] == '|' && charParaLer3[1] == '_')
+                    break;
+                case " __ |__  __|":
+                    Console.Write(5);
+                    break;
+                case " __ |__ |__|":
                     Console.Write(6);
+                    break;
+                case " __    |   |":
+                    Console.Write(7);
+                    break;
+                case " __ |__||__|":
+                    Console.Write(8);
+                    break;
+                case " __ |__| __|":
+                    Console.Write(9);
+                    break;
+                case " __ |  ||__|":
+                    Console.Write(0);
+                    break;
+            }
 
-           
+          
 
-             
-           else if (charParaLer2[0] == ' ' && charParaLer2[1] == ' ' && charParaLer1[1] == '_' && charParaLer1[2] == '_' && charParaLer3[3] == '|' && charParaLer2[3] == '|')
-                Console.Write(7);
+            // idea antiga
+            //if (charParaLer2[0] == ' ' && charParaLer1[1] == ' ' && charParaLer2[3] == '|' && charParaLer3[3] == '|')
+            //    Console.Write(1);
 
-       
-            else if (charParaLer2[0] == '|' && charParaLer2[1] == '_' && charParaLer2[2] == '_' && charParaLer2[3] == '|' && charParaLer1[1] == '_' && charParaLer1[2] == '_' && charParaLer3[0] == '|' && charParaLer3[1] == '_' && charParaLer3[2] == '_' && charParaLer3[3] == '|')
-                Console.Write(8);
 
-       
-    else  if (charParaLer2[0] == '|' && charParaLer2[1] == '_' && charParaLer2[2] == '_' && charParaLer2[3] == '|' && charParaLer1[1] == '_' && charParaLer1[2] == '_' && charParaLer3[0] == ' ' && charParaLer3[1] == '_' && charParaLer3[2] == '_' && charParaLer3[3] == '|')
-        Console.Write(9);
-                else
-                Console.Write(0);
 
-          //  Console.WriteLine(); // habilitar se estiver debugando
+
+            //else if (charParaLer1[1] == '_' && charParaLer1[2] == '_' && charParaLer2[1] == '_' && charParaLer2[0] == ' ' && charParaLer2[2] == '_' && charParaLer2[3] == '|' && charParaLer3[0] == '|' && charParaLer3[1] == '_' && charParaLer3[2] == '_')
+            //    Console.Write(2);
+
+
+
+
+            //else if (charParaLer1[0] == ' ' && charParaLer1[1] == '_' && charParaLer1[2] == '_' && charParaLer1[3] == ' ' && charParaLer2[0] == ' ' && charParaLer2[1] == '_' && charParaLer2[2] == '_' && charParaLer2[3] == '|' && charParaLer3[0] == ' ' && charParaLer3[1] == '_' && charParaLer3[2] == '_' && charParaLer3[3] == '|')
+            //    Console.Write(3);
+
+
+
+
+            //else if (charParaLer2[0] == '|' && charParaLer2[1] == '_' && charParaLer2[2] == '_' && charParaLer2[3] == '|' && charParaLer3[2] == ' ')
+            //    Console.Write(4);
+
+
+
+            //else if (charParaLer1[2] == '_' && charParaLer1[3] == ' ' && charParaLer2[2] == '_' && charParaLer2[3] == ' ' && charParaLer2[1] == '_' && charParaLer3[3] == '|' && charParaLer3[1] == '_' && charParaLer3[0] == ' ')
+            //    Console.Write(5);
+
+            //else if (charParaLer1[2] == '_' && charParaLer1[3] == ' ' && charParaLer2[2] == '_' && charParaLer2[3] == ' ' && charParaLer2[1] == '_' && charParaLer3[3] == '|' && charParaLer3[1] == '_')
+            //    Console.Write(6);
+
+
+
+
+            //else if (charParaLer2[0] == ' ' && charParaLer2[1] == ' ' && charParaLer1[1] == '_' && charParaLer1[2] == '_' && charParaLer3[3] == '|' && charParaLer2[3] == '|')
+            //    Console.Write(7);
+
+
+            //else if (charParaLer2[0] == '|' && charParaLer2[1] == '_' && charParaLer2[2] == '_' && charParaLer2[3] == '|' && charParaLer1[1] == '_' && charParaLer1[2] == '_' && charParaLer3[0] == '|' && charParaLer3[1] == '_' && charParaLer3[2] == '_' && charParaLer3[3] == '|')
+            //    Console.Write(8);
+
+
+            //else if (charParaLer2[0] == '|' && charParaLer2[1] == '_' && charParaLer2[2] == '_' && charParaLer2[3] == '|' && charParaLer1[1] == '_' && charParaLer1[2] == '_' && charParaLer3[0] == ' ' && charParaLer3[1] == '_' && charParaLer3[2] == '_' && charParaLer3[3] == '|')
+            //    Console.Write(9);
+            //else
+            //    Console.Write(0);
+
+            //  Console.WriteLine(); // habilitar se estiver debugando
 
             if ((contador) < linhas[1].Length) // termino os numeros? senão vai pra cima denovo
-            goto checkagem;
+                goto checkagem;
             #endregion
 
             Console.ReadKey();
